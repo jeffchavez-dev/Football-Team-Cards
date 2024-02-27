d23230dc90479e9faaaef1262b8056650c6d4114
Jeff Chavez <67730687+jeffchavez-dev@users.noreply.github.com>
2024-02-27 21:20:38 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 const teamName = document.getElementById("team")
 const typeOfSport = document.getElementById("sport")
 const worldCupYear = document.getElementById("year")
+const headCoach = document.getElementById("head-coach")
