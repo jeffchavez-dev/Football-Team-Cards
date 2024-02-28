885dc0c0969848302dcb758fcbda95171dd0d06b
Jeff Chavez <67730687+jeffchavez-dev@users.noreply.github.com>
2024-02-28 20:37:17 +0800

    Update script.js

--- script.js
+++ script.js
@@ -187,7 +187,9 @@ headCoach.textContent = coachName
 
 const setPlayerCards = (arr = players) => {
     playerCards.innerHTML += arr.map((  {name, position, number, isCaptain, nickname }) => {
-    
+      `
+        <div class="player-card"></div>
+      `
     })
 
 }
