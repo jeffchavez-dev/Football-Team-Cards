8182906bf899988e20b18babc3abfc57bc3f9c3b
Jeff Chavez <67730687+jeffchavez-dev@users.noreply.github.com>
2024-02-28 20:26:34 +0800

    Update script.js

--- script.js
+++ script.js
@@ -178,3 +178,4 @@ Object.freeze(myFavoriteFootballTeam)
 // const team = myFavoriteFootballTeam.team
 
 const { sport, team, year, players } = myFavoriteFootballTeam
+const { coachName } = myFavoriteFootballTeam.headCoach
