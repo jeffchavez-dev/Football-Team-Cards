e0fc27a4222bde5bfd64c8a0a91c618650d4477d
Jeff Chavez <67730687+jeffchavez-dev@users.noreply.github.com>
2024-03-01 22:20:59 +0800

    Update script.js

--- script.js
+++ script.js
@@ -189,7 +189,8 @@ const setPlayerCards = (arr = players) => {
     playerCards.innerHTML += arr.map((  {name, position, number, isCaptain, nickname }) => {
       `
         <div class="player-card">
-          <h2>${name}  ${isCaptain ? '(Captain)' : ""}</h2>
+          <h2>${name} ${isCaptain ? "(Captain)" : ""}</h2>
+          <p>Position: ${position}</p>
         </div>
       `
     })
