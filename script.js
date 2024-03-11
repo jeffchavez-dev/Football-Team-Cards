92a86ddb0d07e5d48539e2e25e8297e084a2684c
Jeff Chavez <67730687+jeffchavez-dev@users.noreply.github.com>
2024-03-11 23:06:46 +0800

    Update script.js

--- script.js
+++ script.js
@@ -204,6 +204,6 @@ playersDropdownList.addEventListener('change', (e) => {
 
     switch(e.target.value){
         case "nickname":
-          setPlayerCards(players.filter((player) => player.nickname !== null)); 
+          setPlayerCards(players.filter((player) => player.nickname !== null)); // implicitly return
     }
 })
